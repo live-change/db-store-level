7d5842cceb50987f4608262df531eacc9f2a10f6
Michał Łaszczewski <michal@emikse.com>
2019-12-23 19:48:51 +0100

    initial commit

---
+++ tests/object-observable.js
@@ -0,0 +1,63 @@
+const test = require('tape')
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+const encoding = require('encoding-down')
+const rimraf = require("rimraf")
+
+const Store = require('../lib/Store.js')
+
+const dbPath = `./test.oo.db`
+rimraf.sync(dbPath)
+const level = levelup(encoding(leveldown(dbPath), { keyEncoding: 'ascii', valueEncoding: 'json' }))
+
+test("store object observable", t => {
+  t.plan(3)
+
+  let store
+
+  t.test("create store", async t => {
+    t.plan(1)
+    store = new Store(level)
+    t.pass('store created')
+  })
+
+  let nextValueResolve
+  const getNextValue = () => new Promise((resolve, reject) => nextValueResolve = resolve)
+
+  let objectObservable
+  const objectObserver = (signal, value, ...rest) => {
+    console.log("SIGNAL", signal, value, ...rest)
+    if(nextValueResolve) nextValueResolve(value)
+  }
+
+  t.test('observe object A', async t => {
+    t.plan(3)
+    objectObservable = store.objectObservable('A')
+    objectObservable.observe(objectObserver)
+    let value = await getNextValue()
+    t.deepEqual(value, null, 'found null')
+
+    t.test("add object A", async t => {
+      t.plan(1)
+      store.put({ id: 'A', a: 1 })
+      let value = await getNextValue()
+      t.deepEqual(value, { id: 'A', a: 1 } , 'found object' )
+    })
+
+    t.test("delete object A", async t => {
+      t.plan(1)
+      store.delete('A')
+      let value = await getNextValue()
+      t.deepEqual(value, null , 'found null' )
+    })
+  })
+
+  t.test("close and remove database", async t => {
+    t.plan(1)
+    await level.close()
+    rimraf(dbPath, (err) => {
+      if(err) return t.fail(err)
+      t.pass('removed')
+    })
+  })
+})
